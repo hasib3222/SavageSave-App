6a880e4f9563759a198a2c7e21a9ba7241dcc888
TurboNest Dev <dev@turbonest.local>
2026-04-29 08:32:07 -0400

    Add auto-updater and bump version to 1.0.1

--- electron/main.js
+++ electron/main.js
@@ -5,7 +5,59 @@
 
 const { app, BrowserWindow, ipcMain, dialog, Notification, clipboard, shell } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
 const { startBackend } = require('../backend/server');
+
+// Configure autoUpdater
+autoUpdater.autoDownload = true;
+autoUpdater.allowPrerelease = false;
+
+function setupAutoUpdater() {
+  autoUpdater.on('checking-for-update', () => {
+    console.log('[Updater] Checking for update...');
+  });
+
+  autoUpdater.on('update-available', (info) => {
+    console.log('[Updater] Update available:', info.version);
+    if (mainWindow) {
+      dialog.showMessageBox(mainWindow, {
+        type: 'info',
+        title: 'Update Available',
+        message: `A new version (${info.version}) is available. It is being downloaded in the background.`,
+      });
+    }
+  });
+
+  autoUpdater.on('update-not-available', () => {
+    console.log('[Updater] Update not available.');
+  });
+
+  autoUpdater.on('error', (err) => {
+    console.error('[Updater] Error:', err);
+  });
+
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ")";
+    console.log('[Updater]', log_message);
+  });
+
+  autoUpdater.on('update-downloaded', (info) => {
+    console.log('[Updater] Update downloaded');
+    dialog.showMessageBox(mainWindow, {
+      type: 'question',
+      buttons: ['Restart Now', 'Later'],
+      defaultId: 0,
+      title: 'Update Ready',
+      message: `Version ${info.version} has been downloaded and is ready to install. Restart now?`,
+    }).then((result) => {
+      if (result.response === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    });
+  });
+}
 
 // Read version from package.json (single source)
 const pkg = require('../package.json');
@@ -78,7 +130,14 @@ ipcMain.handle('shell:showInFolder', (_evt, p) => shell.showItemInFolder(p));
 // Expose backend port (renderer can query it explicitly)
 ipcMain.handle('app:getApiPort', () => backendPort);
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  setupAutoUpdater();
+  // Check for updates after a short delay to allow app to settle
+  setTimeout(() => {
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 3000);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
